edbc92e08ef6069737d91ec74bc28720840a0699
juanca1013 <jmarmol1@my.centennialcollege.ca>
2023-12-25 12:35:48 -0500

    Add banner component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,18 @@
 "use client"
 import { ThemeProvider } from 'next-themes'
 import { NavBar } from './_components/NavBar'
+import { Banner } from './_components/Banner'
+import { ScrollDown } from './_components/ScrollDown'
 
 export default function Home() {
   return (
     <ThemeProvider attribute='class'>
-      <main className="flex min-h-screen flex-col items-center">
-        <NavBar/>
+      <main className="flex min-h-screen flex-col">
+        <div className='min-h-screen'>
+          <NavBar/>
+          <Banner/>
+          <ScrollDown/>
+        </div>
       </main>
     </ThemeProvider>
   )
