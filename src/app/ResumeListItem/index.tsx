f92bc8683d01de99f311eb4e312ac80e9a10c2fa
juanca1013 <jmarmol1@my.centennialcollege.ca>
2023-12-26 13:34:58 -0500

    Create resume list item component

---
+++ src/app/ResumeListItem/index.tsx
@@ -0,0 +1,45 @@
+import { useTheme } from "next-themes";
+import React from "react";
+import { IconType } from "react-icons";
+import { VerticalTimelineElement } from "react-vertical-timeline-component";
+
+export type ResumeItem = {
+  title: string;
+  subtitle: string;
+  description: string;
+  date: string;
+  Icon: IconType;
+  type: "work" | "study";
+};
+
+interface ResumeItemsProps {
+  resumeItem: ResumeItem;
+}
+
+export const ResumeListItem: React.FC<ResumeItemsProps> = ({ resumeItem }) => {
+  const { theme } = useTheme();
+
+  return (
+    <VerticalTimelineElement
+      visible={true}
+      position={resumeItem.type == "work" ? "right" : "left"}
+      className="vertical-timeline-element--work"
+      contentStyle={{
+        background: theme == "light" ? "white" : "rgba(3, 7, 18)",
+        boxShadow: "none",
+      }}
+      contentArrowStyle={{ borderRight: "7px solid  #2793F2" }}
+      date={resumeItem.date}
+      iconStyle={{
+        background: theme == "dark" ? "#0D0D0D" : "rgba(243,244,246)",
+        color: theme == "dark" ? "#fff" : "#0D0D0D",
+      }}
+      dateClassName="text-title"
+      icon={<resumeItem.Icon />}
+    >
+      <h3 className="text-title text-xl">{resumeItem.title}</h3>
+      <h4 className="text-lg">{resumeItem.subtitle}</h4>
+      <p>{resumeItem.description}</p>
+    </VerticalTimelineElement>
+  );
+};
