00412fbbae20753d84b47ae596bfadaf9480f88c
juanca1013 <jmarmol1@my.centennialcollege.ca>
2023-12-28 14:18:12 -0500

    Add Vercel analytics to the page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,31 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import icon from './favicon.ico';
-import './globals.css'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 
-const inter = Inter({ subsets: ['latin'] })
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Juan\'s Portafolio',
-  description: 'Transforming ideas into digital solutions. Discover my expertise in Software Engineering through innovative projects',
-  icons:{
-    icon:'./icon.ico',
-  }
-}
+  title: "Juan's Portafolio",
+  description:
+    "Transforming ideas into digital solutions. Discover my expertise in Software Engineering through innovative projects",
+  icons: {
+    icon: "./icon.ico",
+  },
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Analytics />
+      </body>
     </html>
-  )
+  );
 }
